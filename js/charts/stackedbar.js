8c219de7a192f24d66a57baebf06913e56ac6ffd
unknown <elaheshedaei@gmail.com>
2026-03-02 15:33:20 +0300

    final refinement of charts

--- js/charts/stackedbar.js
+++ js/charts/stackedbar.js
@@ -1,8 +1,9 @@
 async function drawStackedBar() {
 
-  const margin = { top: 40, right: 40, bottom: 40, left: 140 };
+  const margin = { top: 40, right: 80, bottom: 90, left: 80 };
 
   const container = d3.select("#viz-stacked100");
+  container.html("");
   const width = (container.node().getBoundingClientRect().width || 800) - margin.left - margin.right;
   const height = 400 - margin.top - margin.bottom;
 
@@ -59,19 +60,19 @@ async function drawStackedBar() {
     .append("g")
     .attr("fill", d => color(d.key))
     .selectAll("rect")
-    .data(d => d)
+    .data(d => d.map(v => ({ key: d.key, v })))
     .enter()
     .append("rect")
-    .attr("y", d => y(d.data.country))
-    .attr("x", d => x(d[0]))
+    .attr("y", d => y(d.v.data.country))
+    .attr("x", d => x(d.v[0]))
     .attr("height", y.bandwidth())
-    .attr("width", d => x(d[1]) - x(d[0]))
+    .attr("width", d => x(d.v[1]) - x(d.v[0]))
     .on("mouseover", function(event, d) {
     tooltip
       .style("opacity", 1)
       .html(`
-        <strong>${d.data.country}</strong><br/>
-        ${d3.format(".1f")(d[1] - d[0])}%
+        <strong>${d.v.data.country}</strong><br/>
+        <span style="opacity:.8">${d.key}</span>: ${d3.format(".1f")(d.v[1] - d.v[0])}%
       `)
       .style("left", (event.pageX + 15) + "px")
       .style("top", (event.pageY - 28) + "px");
@@ -82,16 +83,34 @@ async function drawStackedBar() {
 
   // Y Axis
   svg.append("g")
-    .call(d3.axisLeft(y))
+    .attr("class", "y-axis")
+    .call(d3.axisLeft(y).tickSizeOuter(0))
     .selectAll("text")
-    .style("fill", "#ccc");
+    .style("fill", "var(--muted)")
+    .style("font-family", "var(--font-mono)")
+    .style("font-size", "12px");
 
   // X Axis
   svg.append("g")
     .attr("transform", `translate(0,${height})`)
-    .call(d3.axisBottom(x).tickFormat(d => d + "%"))
+    .call(
+      d3.axisBottom(x)
+      .tickValues([0, 25, 50, 75, 100])
+      .tickFormat(d => d + "%")
+    )
     .selectAll("text")
     .style("fill", "#ccc");
+
+  svg.append("text")
+  .attr("x", width / 2)
+  .attr("y", height + 45)
+  .attr("text-anchor", "middle")
+  .style("fill", "var(--text)")
+  .style("font-size", "12px")
+  .style("font-family", "var(--font-mono)")
+  .style("font-weight", "500")
+  .style("letter-spacing", "0.5px")
+  .text("Share of recorded events (%)");
 
 }
 
